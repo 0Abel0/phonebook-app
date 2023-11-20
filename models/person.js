88746c3007aa40cb2db55f763ccbb80e5f564722
Abel Gideon <abelgid29@gmail.com>
2023-11-20 21:04:24 +0300

    add validation

--- models/person.js
+++ models/person.js
@@ -4,11 +4,23 @@ const mongoose = require('mongoose')
 const url = process.env.MONGODB_URI
 
 mongoose.set('strictQuery', false)
-mongoose.connect(url)
+mongoose.connect(url).then(result => {
+    console.log("connected successfully")
+}).catch(error => {
+    console.log("something went wrong")
+})
+
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    number: {
+        type: String,
+        required: true
+    }
 })
 
 personSchema.set('toJSON', {
